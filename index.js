4f06734a13f7998c92d17a3172bdfdd94c7396f6
galibcse19 <mrgalib40@gmail.com>
2024-07-08 01:27:39 +0600

    add admin pannel work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express= require('express');
 const app=express();
 const cors =require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 
 //middleWare
@@ -45,7 +46,32 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/users',async(req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
 
+    app.delete('/users/:id',async(req,res)=>{
+      const id= req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter ={_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    })
+
+
+//menu 
     app.get('/menu',async(req,res)=>{
         const result= await menuCollection.find().toArray();
         res.send(result)
